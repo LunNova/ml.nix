3cb404b6307722432844605f0c7dc6284017db9c
Luna Nova <git@lunnova.dev>
2024-12-17 14:10:06 -0800

    composable_kernel: remove unused commented blocks and adjust memory limit

--- rocm-6/composable_kernel/default.nix
+++ rocm-6/composable_kernel/default.nix
@@ -98,29 +98,11 @@ stdenv.mkDerivation (finalAttrs: {
       --replace-fail "add_subdirectory(profiler)" ""
   '';
 
-  # Set up parallel-jobs and compression, then reduce NIX_BUILD_CORES to account for
-  # internal parallelism with parallel-jobs
-  # echo HACK: Removing amdgpu-early-inline-all due to unusably high compile memory.
-  # echo HACK: See https://github.com/llvm/llvm-project/issues/86332
-  # substituteInPlace CMakeLists.txt \
-  #   --replace-fail 'if(NOT WIN32 AND ''${hip_VERSION_FLAT} GREATER 600241132)' 'if(0)' \
-  #   --replace-fail 'amdgpu-early-inline-all' 'invalid-arg-should-not-get-used -invalid'
-
-  # echo HACK: disable CK_USE_AMD_V_DOT_DPP8_INLINE_ASM 1 by replacing in include/ck/ck.hpp to fix gfx908
-  # substituteInPlace include/ck/ck.hpp \
-  #   --replace-fail "CK_USE_AMD_V_DOT_DPP8_INLINE_ASM 1" "CK_USE_AMD_V_DOT_DPP8_INLINE_ASM 0"
-
-  #export MALLOC_CONF="prof:true,lg_prof_sample:1,prof_accum:false,prof_prefix:jeprof.out"
-  #export MALLOC_CONF="background_thread:true,narenas:2,dirty_decay_ms:5000,muzzy_decay_ms:5000"
-  # export MALLOC_CONF="narenas:1,tcache:true,background_thread:true,dirty_decay_ms:1000,muzzy_decay_ms:1000"
-  # export LD_PRELOAD="${jemalloc}/lib/libjemalloc.so";
-
-
   # Clamp parallelism based on free memory at build start to avoid OOM
   preConfigure = ''
-    MEM_GB_FREE=$(awk '/MemFree/ { printf "%d \n", $2/1024/1024 }' /proc/meminfo)
-    MAX_CORES=$((1 + MEM_GB_FREE / 10))
-    MAX_CORES_LINK=$((1 + MEM_GB_FREE / 50))
+    MEM_GB_FREE=$(awk '/MemAvailable/ { printf "%d \n", $2/1024/1024 }' /proc/meminfo)
+    MAX_CORES=$((1 + MEM_GB_FREE / 4))
+    MAX_CORES_LINK=$((1 + MEM_GB_FREE / 8))
     MAX_CORES_LINK=$((MAX_CORES_LINK > NIX_BUILD_CORES ? NIX_BUILD_CORES : MAX_CORES_LINK))
     export NIX_BUILD_CORES="$((1 + NIX_BUILD_CORES / 2))"
     export NIX_BUILD_CORES="$((NIX_BUILD_CORES > MAX_CORES ? MAX_CORES : NIX_BUILD_CORES))"
@@ -147,9 +129,6 @@ stdenv.mkDerivation (finalAttrs: {
     inherit (finalAttrs.src) owner;
     inherit (finalAttrs.src) repo;
   };
-
-  # This will use a lot of RAM per core, set a small --cores if you OOM.
-  requiredSystemFeatures = [ "big-parallel" ];
 
   meta = with lib; {
     description = "Performance portable programming model for machine learning tensor operators";
